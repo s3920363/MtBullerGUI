feb2918e27095ebb785fc42059f7a82c4794b3bd
Ricky Nguyen <s3920363@student.rmit.edu.au>
2025-10-08 21:12:16 +1100

    Rename MtBullerResortGUI class to GUI in main method

--- src/MtBullerAdmin.java
+++ src/MtBullerAdmin.java
@@ -4,6 +4,7 @@ public class MtBullerAdmin {
     public static void main(String[] args) {
         MtBullerResort mt = new MtBullerResort();
         mt.populateLists();
-        SwingUtilities.invokeLater(() -> new MtBullerResortGUI().setVisible(true));
+        SwingUtilities.invokeLater(() -> new GUI().setVisible(true));
+//        mt.run();
     }
 }
