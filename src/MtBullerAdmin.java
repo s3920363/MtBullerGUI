0fe3986d14c36b57fdb4eae35f414ca10d7304b2
Ricky Nguyen <s3920363@student.rmit.edu.au>
2025-10-11 03:43:17 +1100

    Refactor MtBullerAdmin to run the resort logic directly in main method

--- src/MtBullerAdmin.java
+++ src/MtBullerAdmin.java
@@ -1,10 +1,7 @@
-import javax.swing.*;
-
 public class MtBullerAdmin {
     public static void main(String[] args) {
         MtBullerResort mt = new MtBullerResort();
         mt.populateLists();
-        SwingUtilities.invokeLater(() -> new GUI().setVisible(true));
-//        mt.run();
+        mt.run();
     }
 }
